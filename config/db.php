27af4b17a712534d120ea58ce73c4540601c02b7
er361 <sf7kmmr@gmail.com>
2017-09-06 09:30:06 +0600

    start project

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=ove4ki',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
